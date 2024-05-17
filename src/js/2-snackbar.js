6a81598253bfddc9cc332fe05c4dd2346bb3b775
Eva-Mykhno <154619097+Eva-Mykhno@users.noreply.github.com>
2024-05-17 17:00:37 +0300

    task-2

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,7 +1,60 @@
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
-const myForm = document.querySelector('form');
-const msInput = document.querySelector('input');
-const fulfilled = document.querySelector('input[value="fulfilled"]');
-const rejected = document.querySelector('input[value="rejected"]');
+const myForm = document.querySelector('.form');
+const inputDelay = document.querySelector('input[name="delay"]');
+const createBtn = document.querySelector('button');
+inputDelay.classList.add('input-delay');
+createBtn.classList.add('create-button');
+
+console.log(createBtn);
+myForm.addEventListener('submit', createPromises);
+
+function createPromises(event) {
+  event.preventDefault();
+
+  const selectState = document.querySelector('input[name="state"]:checked');
+
+  const delay = +inputDelay.value;
+  const select = selectState.value;
+
+  if (delay <= 0) {
+    return;
+  }
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (select === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+
+  promise
+    .then(delay => {
+      iziToast.show({
+        message: `Fulfilled promise in ${delay}ms`,
+        iconUrl: './img/circle.svg',
+        color: '#FFFFFF',
+        messageColor: '#FFFFFF',
+        backgroundColor: '#59A10D',
+        messageSize: '16',
+        position: 'topRight',
+        theme: 'dark',
+      });
+    })
+    .catch(delay => {
+      iziToast.show({
+        message: ` Rejected promise in ${delay}ms`,
+        color: '#FFFFFF',
+        messageColor: '#FFFFFF',
+        backgroundColor: '#EF4040',
+        messageSize: '16',
+        position: 'topRight',
+        theme: 'dark',
+        iconUrl: './img/octagon.svg',
+      });
+    });
+  myForm.reset();
+}
